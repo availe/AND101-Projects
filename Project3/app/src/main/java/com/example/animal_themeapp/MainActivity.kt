2a2326b8fdeb494801c9e16365bda5107b0b69d8
availe <94802525+availe@users.noreply.github.com>
2023-10-06 01:41:00 -0400

    project3_upload

---
+++ Project3/app/src/main/java/com/example/animal_themeapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.animal_themeapp
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
